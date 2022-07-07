fe36b3564d4b4ca004d3ecaea30a2b85a45538e9
Vladislav <nicochan00@mail.ru>
2022-07-08 01:53:15 +0300

    ending project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const body = document.body;
+const click = document.querySelector('#click');
+
+click.addEventListener('click', isChecked);
+
+function isChecked() {
+   if (click.checked) {
+      body.classList.add('lock');
+   } else {
+      body.classList.remove('lock');
+   }
+}
